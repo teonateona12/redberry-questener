7fda91bd4d91b83dfe635f38cafe1cd45a59ccfb
Vato Loladze <a.loladze16@gmail.com>
2023-04-05 16:51:27 +0400

    added routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,20 @@
+import { Route, Routes} from 'react-router-dom'
+import {Covid, Office, Personal, Start, Thanks, Vaccine } from './pages'
+
+
 function App() {
-  return <div className=""></div>;
+  return (
+    <>
+      <Routes>
+        <Route path='/' element={<Start/>}/>
+        <Route path='/covid' element={<Covid/>}/>
+        <Route path='/office' element={<Office/>}/>
+        <Route path='/personal' element={<Personal/>}/>
+        <Route path='/start' element={<Thanks/>}/>
+        <Route path='/vaccine' element={<Vaccine/>}/>
+      </Routes>
+    </>
+  )
 }
 
 export default App;
