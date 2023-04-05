0116165f584aa4d833a49b6d9c698a21febc6823
Vato Loladze <a.loladze16@gmail.com>
2023-04-05 20:48:15 +0400

    feat: added start page content

--- src/pages/Start/Start.jsx
+++ src/pages/Start/Start.jsx
@@ -1,5 +1,15 @@
+import { Logo } from "../../assets";
+
 const Start = () => {
-  return <div></div>;
-};
+  return (
+    <div className=" w-screen h-screen inline-flex flex-col items-center justify-center">
+      <img src={Logo} alt="" className="mb-20" />
+      <button className="realtive w-80 h-40 inline-flex items-top justify-center">
+        <h4 className="w-40 absolute font-bold text-3xl text-white stroke-black translate-y-0.5 translate-x-0.5 opacity-0">კითხვარის დაწყება</h4>
+        <h4 className="w-40 font-bold text-3xl absolute">კითხვარის დაწყება</h4>
+      </button>
+    </div>
+  );
+}
 
 export default Start;
