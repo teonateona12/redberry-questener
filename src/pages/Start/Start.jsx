8b77a398f6f6d10b7f365cc73f12b56a408c483c
Vato Loladze <a.loladze16@gmail.com>
2023-04-06 17:16:14 +0400

    fix: added alt to img on start page

--- src/pages/Start/Start.jsx
+++ src/pages/Start/Start.jsx
@@ -4,7 +4,7 @@ import { Link} from 'react-router-dom'
 const Start = () => {
   return (
     <div className="w-screen h-screen bg-bgMain inline-flex flex-col items-center justify-center">
-      <img src={Logo} alt="" className="animate-shrink mb-20" />
+      <img src={Logo} alt="redberry-logo" className="animate-shrink mb-20" />
       <Link to={'/personal'} >
           <button className="animate-appear group realtive w-80 h-40 inline-flex items-top justify-center">
             <h4 style={styledStroke} className="w-40 absolute font-bold text-3xl text-white stroke-black translate-y-0.5 translate-x-0.5 opacity-0 group-hover:opacity-100 transition-all">კითხვარის დაწყება</h4>
