e34dc42e54cfa97b21d45a4b03cf60ddb92a72ae
WhereIsPumpkin <sgogr2021@agruni.edu.ge>
2023-04-11 02:22:30 +0400

    implement second questionary

--- src/pages/Covid/Covid.jsx
+++ src/pages/Covid/Covid.jsx
@@ -4,75 +4,126 @@ import FormHeader from "../../components/FormHeader";
 
 const Covid = () => {
   const [covidStatus, setCovidStatus] = useState("");
+  const [antibodyStatus, setAntibodyStatus] = useState("");
 
   const handleCovidStatusChange = (event) => {
     setCovidStatus(event.target.value);
-    // handle form submission
+  };
+  const handleAntibodyStatusChange = (event) => {
+    setAntibodyStatus(event.target.value);
   };
 
   return (
     <div className="w-full h-full flex flex-col px-[200px] bg-[#EAEAEA;]">
       <FormHeader partition={2} />
-      <div className="formWrapper">
-        <form className="mt-[42px] flex flex-col gap-[18px]">
-          <label className="font-bold text-[24px] leading-6 text-[#232323]">
-            გაქვს გადატანილი Covid-19?*
-          </label>
-
-          <div className="flex items-center ml-[19px]">
-            <input
-              type="radio"
-              name="covidStatus"
-              id="covid-yes"
-              value="yes"
-              checked={covidStatus === "yes"}
-              onChange={handleCovidStatusChange}
-              className="appearance-none w-[23px] h-[23px] rounded-full border border-[#232323]  checked:border-[#232323]"
-            />
-            <label
-              htmlFor="covid-yes"
-              className="ml-[19px] font-[400] text-[20px] text-[#000000]"
-            >
-              კი
+      <div className="flex justify-between">
+        <div className="flex flex-col">
+          <form className="mt-[42px] flex flex-col gap-[18px]">
+            <label className="font-bold text-[22px] leading-6 text-[#232323]">
+              გაქვს გადატანილი Covid-19?*
             </label>
-          </div>
 
-          <div className="flex items-center ml-[19px]">
-            <input
-              type="radio"
-              name="covidStatus"
-              id="covid-no"
-              value="no"
-              checked={covidStatus === "no"}
-              onChange={handleCovidStatusChange}
-              className="appearance-none w-[23px] h-[23px] rounded-full border border-[#232323]  checked:border-[#232323]"
-            />
-            <label
-              htmlFor="covid-no"
-              className="ml-[19px] font-[400] text-[20px] text-[#000000]"
-            >
-              არა
-            </label>
-          </div>
+            <div className="flex items-center ml-[19px]">
+              <input
+                type="radio"
+                name="covidStatus"
+                id="covid-yes"
+                value="yes"
+                checked={covidStatus === "yes"}
+                onChange={handleCovidStatusChange}
+                className="appearance-none w-[23px] h-[23px] rounded-full border border-[#232323]  checked:border-[#232323]"
+              />
+              <label
+                htmlFor="covid-yes"
+                className="ml-[19px] font-[400] text-[20px] text-[#000000]"
+              >
+                კი
+              </label>
+            </div>
 
-          <div className="flex items-center ml-[19px]">
-            <input
-              type="radio"
-              name="covidStatus"
-              id="covid-currently"
-              value="currently"
-              checked={covidStatus === "currently"}
-              onChange={handleCovidStatusChange}
-              className="appearance-none w-[23px] h-[23px] rounded-full border border-[#232323]  checked:border-[#232323]"
-            />
-            <label
-              htmlFor="covid-currently"
-              className="ml-[19px] font-[400] text-[20px] text-[#000000]"
-            >
-              ახლა მაქვს
-            </label>
-          </div>
-        </form>
+            <div className="flex items-center ml-[19px]">
+              <input
+                type="radio"
+                name="covidStatus"
+                id="covid-no"
+                value="no"
+                checked={covidStatus === "no"}
+                onChange={handleCovidStatusChange}
+                className="appearance-none w-[23px] h-[23px] rounded-full border border-[#232323]  checked:border-[#232323]"
+              />
+              <label
+                htmlFor="covid-no"
+                className="ml-[19px] font-[400] text-[20px] text-[#000000]"
+              >
+                არა
+              </label>
+            </div>
+
+            <div className="flex items-center ml-[19px]">
+              <input
+                type="radio"
+                name="covidStatus"
+                id="covid-currently"
+                value="currently"
+                checked={covidStatus === "currently"}
+                onChange={handleCovidStatusChange}
+                className="appearance-none w-[23px] h-[23px] rounded-full border border-[#232323]  checked:border-[#232323]"
+              />
+              <label
+                htmlFor="covid-currently"
+                className="ml-[19px] font-[400] text-[20px] text-[#000000]"
+              >
+                ახლა მაქვს
+              </label>
+            </div>
+          </form>
+
+          {covidStatus === "yes" && (
+            <form className="mt-[42px] flex flex-col gap-[18px]">
+              <label className="font-bold text-[22px] leading-6 text-[#232323]">
+                ანტისხეულების ტესტი გაქვს გაკეთებული?*
+              </label>
+
+              <div className="flex items-center ml-[19px]">
+                <input
+                  type="radio"
+                  name="antibodyStatus"
+                  id="antibody-yes"
+                  value="yes"
+                  checked={antibodyStatus === "yes"}
+                  onChange={handleAntibodyStatusChange}
+                  className="appearance-none w-[23px] h-[23px] rounded-full border border-[#232323]  checked:border-[#232323]"
+                />
+                <label
+                  htmlFor="antibody-yes"
+                  className="ml-[19px] font-[400] text-[20px] text-[#000000]"
+                >
+                  კი
+                </label>
+              </div>
+
+              <div className="flex items-center ml-[19px]">
+                <input
+                  type="radio"
+                  name="antibodyStatus"
+                  id="antibody-no"
+                  value="no"
+                  checked={antibodyStatus === "no"}
+                  onChange={handleAntibodyStatusChange}
+                  className="appearance-none w-[23px] h-[23px] rounded-full border border-[#232323]  checked:border-[#232323]"
+                />
+                <label
+                  htmlFor="antibody-no"
+                  className="ml-[19px] font-[400] text-[20px] text-[#000000]"
+                >
+                  არა
+                </label>
+              </div>
+            </form>
+          )}
+        </div>
+
+        <img src={temperature} alt="man" />
       </div>
     </div>
   );
