88b6de8ba87c7586dd229ae543d6b9074184d399
Vato Loladze <a.loladze16@gmail.com>
2023-04-11 15:27:00 +0400

    feat: add navigation btns

--- src/pages/Vaccine/Vaccine.jsx
+++ src/pages/Vaccine/Vaccine.jsx
@@ -1,9 +1,21 @@
+import { useNavigate } from "react-router-dom";
 import FormHeader from "../../components/FormHeader";
+import { ArrowLeft , ArrowRight, Doctor } from "../../assets";
+import { useState} from "react";
+
 
 const Vaccine = () => {
+  const [canAdvance, setCanAdvance] = useState(false);
+  const navigate = useNavigate();
+
   return (
     <div>
       <FormHeader partition={3}/> 
+
+      <div className="flex items-center justify-center">
+        <button onClick={() => navigate("/covid")} className="mr-[117px]"><img src={ArrowLeft} alt=""/></button>
+        <button onClick={() => navigate("/office")} disabled={!canAdvance}><img src={ArrowRight} className={canAdvance? "filter brightness-0": ""} alt="" /></button>
+      </div>
     </div>
   );
 };
