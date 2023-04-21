d9bcf94744b29a6e5b2cf41dde8f07934087c7f8
Vato Loladze <a.loladze16@gmail.com>
2023-04-21 11:48:33 +0400

    fix: tailwind styles rewrite

--- src/pages/Vaccine/Vaccine.jsx
+++ src/pages/Vaccine/Vaccine.jsx
@@ -18,7 +18,6 @@ const Vaccine = () => {
     hasMounted? localStorage.setItem('localUser', JSON.stringify(applicant)) : setHasMounted(true);
   }, [applicant, hasMounted]);
   const handleDosage = (e) => {
-    setCanAdvance(true)
     dispach(updateData({property: "vaccination_stage", value: e.target.value}))
   }
   const {register, handleSubmit, setValue, formState: {errors}} = new useForm({
@@ -35,55 +34,55 @@ const Vaccine = () => {
   }
   return (
     <form onSubmit={handleSubmit(onValid)} className="px-[200px] w-full h-full bg-bgMain text-primaryText overflow-x-hidden">
-      <FormHeader partition={3}/>
-      <div className="flex justify-between items-top">
-        <div>
-          <div className="flex flex-col justify-center mt-[42px]">
-            <label className="font-bold text-[22px] my-[8px]">უკვე აცრილი ხარ?*</label>
-            <div className="flex flex-col justify-center pl-6">
-              <label className="flex items-center h-[40px] text-xl">
-                <input {...register("had_vaccine")}  onChange={handleCovidChange} type="radio" className="mr-[19px] appearance-none w-[23px] h-[23px] rounded-full border border-[#232323] checked:border-[#232323]" value="true" checked={applicant.had_vaccine===true} name="had_vaccine"/>
-                კი
-              </label>
-              <label className="flex items-center h-[40px] text-xl">
-                <input {...register("had_vaccine")}  onChange={handleCovidChange} type="radio" className="mr-[19px] appearance-none w-[23px] h-[23px] rounded-full border border-[#232323] checked:border-[#232323]" value="false" checked={applicant.had_vaccine===false} name="had_vaccine"/>
-                არა
-              </label>
-              <p className={applicant.had_vaccine!==null?"hidden":"text-error"}>{errors.had_vaccine?.message}</p>
-            </div>
-          </div>
-          {applicant.had_vaccine == null? "" : (applicant.had_vaccine? (
-            <div className="flex flex-col justify-center mt-[46px]">
-            <label className="font-bold text-[22px] my-[8px]">აირჩიე რა ეტაპზე ხარ*</label>
-            <div className="flex flex-col justify-center pl-6">
-               <label className="flex items-center h-[40px] text-xl">
-                 <input {...register("vaccination_stage")} onChange={handleDosage} type="radio" className="mr-[19px] appearance-none w-[23px] h-[23px] rounded-full border border-[#232323] checked:border-[#232323]" value="first_dosage_and_registered_on_the_second" checked={applicant.vaccination_stage==="first_dosage_and_registered_on_the_second"} name="vaccination_stage"/>
-                 პირველი დოზა და დარეგისტრირებული ვარ მეორეზე
-               </label>
-               <label className="flex items-center h-[40px] text-xl">
-                 <input {...register("vaccination_stage")} onChange={handleDosage} type="radio" className="mr-[19px] appearance-none w-[23px] h-[23px] rounded-full border border-[#232323] checked:border-[#232323]" value="fully_vaccinated" checked={applicant.vaccination_stage==="fully_vaccinated"} name="vaccination_stage"/>
+  <FormHeader partition={3}/>
+  <div className="flex justify-between items-top">
+    <div>
+      <div className="flex flex-col justify-center mt-10">
+        <label className="font-bold text-2xl my-2">უკვე აცრილი ხარ?*</label>
+        <div className="flex flex-col justify-center pl-6">
+          <label className="flex items-center h-10 text-xl">
+            <input {...register("had_vaccine")}  onChange={handleCovidChange} type="radio" className="mr-5 appearance-none w-6 h-6 rounded-full border border-gray-900 checked:border-gray-900" value="true" checked={applicant.had_vaccine===true} name="had_vaccine"/>
+            კი
+          </label>
+          <label className="flex items-center h-10 text-xl">
+            <input {...register("had_vaccine")}  onChange={handleCovidChange} type="radio" className="mr-5 appearance-none w-6 h-6 rounded-full border border-gray-900 checked:border-gray-900" value="false" checked={applicant.had_vaccine===false} name="had_vaccine"/>
+            არა
+          </label>
+          <p className={applicant.had_vaccine!==null?"hidden":"text-error"}>{errors.had_vaccine?.message}</p>
+        </div>
+      </div>
+      {applicant.had_vaccine == null? "" : (applicant.had_vaccine? (
+        <div className="flex flex-col justify-center mt-12">
+        <label className="font-bold text-2xl my-2">აირჩიე რა ეტაპზე ხარ*</label>
+        <div className="flex flex-col justify-center pl-6">
+           <label className="flex items-center h-10 text-xl">
+             <input {...register("vaccination_stage")} onChange={handleDosage} type="radio" className="mr-5 appearance-none w-6 h-6 rounded-full border border-gray-900 checked:border-gray-900" value="first_dosage_and_registered_on_the_second" checked={applicant.vaccination_stage==="first_dosage_and_registered_on_the_second"} name="vaccination_stage"/>
+             პირველი დოზა და დარეგისტრირებული ვარ მეორეზე
+           </label>
+           <label className="flex items-center h-10 text-xl">
+             <input {...register("vaccination_stage")} onChange={handleDosage} type="radio" className="mr-5 appearance-none w-6 h-6 rounded-full border border-gray-900 checked:border-gray-900" value="fully_vaccinated" checked={applicant.vaccination_stage==="fully_vaccinated"} name="vaccination_stage"/>
                  სრულად აცრილი ვარ
-               </label>
-               <label className="flex items-center h-[40px] text-xl">
-                 <input {...register("vaccination_stage")} onChange={handleDosage} type="radio" className="mr-[19px] appearance-none w-[23px] h-[23px] rounded-full border border-[#232323] checked:border-[#232323]" value="first_dosage_and_not_registered_on_the_second" checked={applicant.vaccination_stage==="first_dosage_and_not_registered_on_the_second"} name="vaccination_stage"/>
+           </label>
+           <label className="flex items-center h-10 text-xl">
+             <input {...register("vaccination_stage")} onChange={handleDosage} type="radio" className="mr-5 appearance-none w-6 h-6 rounded-full border border-gray-900 checked:border-gray-900" value="first_dosage_and_not_registered_on_the_second" checked={applicant.vaccination_stage==="first_dosage_and_not_registered_on_the_second"} name="vaccination_stage"/>
                  პირველი დოზა და არ დავრეგისტრირებულვარ მეორეზე
-               </label>
+           </label>
             </div>
             </div>            
           ) : (
-            <div className="flex flex-col justify-center mt-[46px]">
-            <label className="font-bold text-[22px] my-[8px]">რას ელოდები?*</label>
+            <div className="flex flex-col justify-center mt-12">
+            <label className="font-bold text-2xl my-2">რას ელოდები?*</label>
             <div className="flex flex-col justify-center pl-6">
-               <label className="flex items-center  h-[40px] text-xl">
-                 <input {...register("vaccination_stage")} onChange={e => {}} onClick={handleDosage} type="radio" className="mr-[19px] appearance-none w-[23px] h-[23px] rounded-full border border-[#232323] checked:border-[#232323]" value="registered_and_waiting" checked={applicant.vaccination_stage==="registered_and_waiting"} name="vaccination_stage"/>
+               <label className="flex items-center h-10 text-xl">
+                 <input {...register("vaccination_stage")} onChange={e => {}} onClick={handleDosage} type="radio" className="mr-5 appearance-none w-6 h-6 rounded-full border border-gray-900 checked:border-gray-900" value="registered_and_waiting" checked={applicant.vaccination_stage==="registered_and_waiting"} name="vaccination_stage"/>
                  დარეგისტრირებული ვარ და ველოდები რიცხვს
                </label>
-               <label className="flex items-center h-[40px] text-xl">
-                 <input {...register("vaccination_stage")} onChange={e => {}} onClick={handleDosage} type="radio" className="mr-[19px] appearance-none w-[23px] h-[23px] rounded-full border border-[#232323] checked:border-[#232323]" value="not_planning" checked={applicant.vaccination_stage==="not_planning"} name="vaccination_stage"/>
+               <label className="flex items-center h-10 text-xl">
+                 <input {...register("vaccination_stage")} onChange={e => {}} onClick={handleDosage} type="radio" className="mr-5 appearance-none w-6 h-6 rounded-full border border-gray-900 checked:border-gray-900" value="not_planning" checked={applicant.vaccination_stage==="not_planning"} name="vaccination_stage"/>
                  არ ვგეგმავ
                </label>
-               <label className="flex items-center h-[40px] text-xl">
-                 <input {...register("vaccination_stage")} onChange={e => {}} onClick={handleDosage} type="radio" className="mr-[19px] appearance-none w-[23px] h-[23px] rounded-full border border-[#232323] checked:border-[#232323]" value="already_had_it_and_not_planning" checked={applicant.vaccination_stage==="already_had_it_and_not_planning"} name="vaccination_stage"/>
+               <label className="flex items-center h-10 text-xl">
+                 <input {...register("vaccination_stage")} onChange={e => {}} onClick={handleDosage} type="radio" className="mr-5 appearance-none w-6 h-6 rounded-full border border-gray-900 checked:border-gray-900" value="already_had_it_and_not_planning" checked={applicant.vaccination_stage==="already_had_it_and_not_planning"} name="vaccination_stage"/>
                  გადატანილი მაქვს და ვგეგმავ აცრას
                </label>
             </div>
@@ -91,27 +90,27 @@ const Vaccine = () => {
           ))}
           <p className={applicant.had_vaccine === null || applicant.vaccination_stage !== null? "hidden" : "text-error ml-6"}>{errors.vaccination_stage?.message}</p>
           {applicant.vaccination_stage=="first_dosage_and_not_registered_on_the_second"? (
-            <p className="w-[491px] pl-[67px] text-xl mt-[39px]">
+            <p className="w-96 pl-16 text-xl mt-10">
               რომ არ გადადო, <br/>
               ბარემ ახლავე დარეგისტრირდი
               <a className="block text-link" href="https://booking.moh.gov.ge/">https://booking.moh.gov.ge/</a>
             </p>
           ):""}
           {applicant.vaccination_stage=="already_had_it_and_not_planning"? (
-            <div className="w-[550px] pl-[67px] text-xl mt-[39px]">
+            <div className="w-112 pl-16 text-xl mt-10">
               <p>ახალი პროტოკოლით კოვიდის გადატანიდან 1 თვის შემდეგ შეგიძლიათ ვაქცინის გაკეთება.</p>
               <p>👉 რეგისტრაციის ბმული</p>
               <a className="block text-link" href="https://booking.moh.gov.ge/">https://booking.moh.gov.ge/</a>
             </div>          
           ):""}
           {applicant.vaccination_stage=="not_planning"? (
-            <a className="block text-link pl-[67px] text-xl mt-[39px]" href="https://booking.moh.gov.ge/">https://booking.moh.gov.ge/</a>
+            <a className="block text-link pl-16 text-xl mt-10" href="https://booking.moh.gov.ge/">https://booking.moh.gov.ge/</a>
           ):""}
         </div>
-        <img className="max-h-[748px] mt-[36px]" src={Doctor} alt="" />
+        <img className="mt-9" src={Doctor} alt="" />
       </div>
       <div className="flex items-center justify-center">
-        <button onClick={() => navigate("/covid")} className="mr-[117px]"><img src={ArrowLeft} alt=""/></button>
+        <button onClick={() => navigate("/covid")} className="mr-28"><img src={ArrowLeft} alt=""/></button>
         <button type="submit"><img src={ArrowRight} className={applicant.had_vaccine!== null&& applicant.vaccination_stage!==null? "filter brightness-0": ""} alt="" /></button>
       </div>
     </form>
