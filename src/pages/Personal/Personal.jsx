13d1afe92fb9d0c264f1fa0b8be743139f86765c
khantadzeeren <miusern1@gmail.com>
2023-04-29 13:37:07 +0400

    fix: fixed localstorage

--- src/pages/Personal/Personal.jsx
+++ src/pages/Personal/Personal.jsx
@@ -2,53 +2,62 @@ import { Persons } from "../../assets";
 import FormHeader from "../../components/FormHeader";
 import { ArrowRight } from "../../assets";
 import { useNavigate } from "react-router-dom";
-import { useForm  } from "react-hook-form";
-import * as yup from 'yup'
-import {yupResolver} from '@hookform/resolvers/yup'
+import { useForm } from "react-hook-form";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
 import { useEffect } from "react";
 import axios from "axios";
 
-
-const saveFormDataToLocalStorage = (formData) => {
-  localStorage.setItem('formData', JSON.stringify(formData));
-};
-
-
 const Personal = () => {
   const navigate = useNavigate();
-  
+
   const PersonalSchema = yup.object().shape({
-    name: yup.string().required("სახელის ველის შევსება სავალდებულოა").min(2, "სახელის ველი უნდა შედგებოდეს მინიმუმ 3 სიმბოლოსგან").max(255, "სახელის ველი უნდა შედგებოდეს მაქსიმუმ 255 სიმბოლოსგან" ) ,
-    email: yup.string().email("თქვენ მიერ შეყვანილი მეილი არასწორია").required("მეილის ველის შევსება სავალდებულოა").matches(/@redberry\.ge$/, "გთხოვთ დარეგისტრირდეთ Redberry-ს მეილით (youremail@redberry.ge)"  ),
-    surname: yup.string().required("გვარის ველის შევსება სავალდებულოა").min(2, "გვარის ველი უნდა შედგებოდეს მინიმუმ 3 სიმბოლოსგან").max(255, "გვარის ველი უნდა შედგებოდეს მაქსიმუმ 255 სიმბოლოსგან"),
-  })
+    name: yup
+      .string()
+      .required("სახელის ველის შევსება სავალდებულოა")
+      .min(2, "სახელის ველი უნდა შედგებოდეს მინიმუმ 3 სიმბოლოსგან")
+      .max(255, "სახელის ველი უნდა შედგებოდეს მაქსიმუმ 255 სიმბოლოსგან"),
+    email: yup
+      .string()
+      .email("თქვენ მიერ შეყვანილი მეილი არასწორია")
+      .required("მეილის ველის შევსება სავალდებულოა")
+      .matches(
+        /@redberry\.ge$/,
+        "გთხოვთ დარეგისტრირდეთ Redberry-ს მეილით (youremail@redberry.ge)"
+      ),
+    surname: yup
+      .string()
+      .required("გვარის ველის შევსება სავალდებულოა")
+      .min(2, "გვარის ველი უნდა შედგებოდეს მინიმუმ 3 სიმბოლოსგან")
+      .max(255, "გვარის ველი უნდა შედგებოდეს მაქსიმუმ 255 სიმბოლოსგან"),
+  });
 
+  const {
+    register,
+    handleSubmit,
+    setValue,
+    getValues,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(PersonalSchema),
+  });
 
-    const {  register,handleSubmit,setValue, formState: { errors } } = useForm({
-      resolver: yupResolver(PersonalSchema),
-      
-    });
-  
-    const onSubmit = async (data) => {
-      // const response = await axios.post('https://covid19.devtest.ge/api/create', data);
-      // console.log(response.data);
-      saveFormDataToLocalStorage(data);
-      navigate("/Covid");
-    };
-    const saveFormDataToLocalStorage = (formData) => {
-      localStorage.setItem('formData', JSON.stringify(formData));
-    };
-    
-    useEffect(() => {
-      const formData = JSON.parse(localStorage.getItem('formData'));
-    
-      if (formData) {
-        Object.keys(formData).forEach((key) => {
-          setValue(key, formData[key]);
-        });
-      }
-    }, []);
-    
+  const onSubmit = async (data) => {
+    navigate("/Covid");
+  };
+  const saveFormDataToLocalStorage = (formData) => {
+    localStorage.setItem("formData", JSON.stringify(formData));
+  };
+
+  useEffect(() => {
+    const formData = JSON.parse(localStorage.getItem("formData"));
+
+    if (formData) {
+      Object.keys(formData).forEach((key) => {
+        setValue(key, formData[key]);
+      });
+    }
+  }, []);
 
   return (
     <div className="pl-[200px] pr-[165px] w-full h-full ">
@@ -62,20 +71,21 @@ const Personal = () => {
               <input
                 className="mt-[1px] border-2 border-[#232323]   "
                 type="text"
-                {...register("name",
-                 
-                 )}
+                {...register("name")}
+                onChange={(e) =>
+                  saveFormDataToLocalStorage({ ...getValues(), name: e.target.value })}
               />
-              <p className="text-red-500" >{errors.name?.message}</p>
+              <p className="text-red-500">{errors.name?.message}</p>
             </div>
             <div className="mt-[47px]">
               <h2>გვარი*</h2>
               <input
                 className="mt-[1px] border-2 border-[#232323]"
                 type="text"
-                {...register("surname" ,
-                
-                )}
+                {...register("surname")}
+                onChange={(e) =>
+                  saveFormDataToLocalStorage({ ...getValues(), surname: e.target.value })}
+                  
               />
               <p className="text-red-500">{errors.surname?.message}</p>
             </div>
@@ -84,12 +94,11 @@ const Personal = () => {
               <input
                 className="mt-[1px] border-2 border-[#232323]"
                 type="email"
-                {...register("email",
-                
-                )}
-
+                {...register("email")}
+                onChange={(e) =>
+                  saveFormDataToLocalStorage({ ...getValues(), email: e.target.value })}
               />
-              <p className="text-red-500" >{errors.email?.message}</p>
+              <p className="text-red-500">{errors.email?.message}</p>
             </div>
             <hr className=" mt-[111px]" />
             <div className="">
@@ -101,12 +110,13 @@ const Personal = () => {
             <img className="ml-[138px]  mt-[-50px]" src={Persons} alt="" />
           </div>
         </div>
-        <button  type="submit"  className="flex justify-center items-center  ">
-        <img   src={ArrowRight} alt="" />
-      </button>
+        <div className="flex justify-center items-center">
+          <button type="submit" className=" m-auto">
+          <img src={ArrowRight} alt="" />
+        </button>
+        </div>
+        
       </form>
-
-      
     </div>
   );
 };
